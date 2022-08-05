f72cb275bdd50d311e823d194f5597c04d015e5d
kaatarn-protecs <golddez@gmail.com>
2022-08-05 11:23:42 +0300

    Добавили папку для первой задачи

--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@
 // -3 -> 9
 // -7 -> 49
 
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num*num;
+Console.Write($"Квадрат числа: {num} = {square}" );
